4973977df15122df9eab22ddac30628be0a9e515
BadPixxel <eshop.bpaquier@gmail.com>
2018-12-14 11:09:47 +0100

    PhpUnit Only

--- src/Resources/travis/install.sh
+++ src/Resources/travis/install.sh
@@ -9,5 +9,11 @@ composer update  --prefer-dist --no-interaction
 echo "Install Symfony"
 php bin/console doctrine:schema:update --force  --no-interaction --no-debug
 
+echo "Link Symfony Test Container Xml"
+# Symfony 2 & 3
+if [ -f var/cache/dev/appDevDebugProjectContainer.xml ]; then cp var/cache/dev/appDevDebugProjectContainer.xml var/cache/dev/testContainer.xml; fi;
+# Symfony 4+
+if [ -f var/cache/dev/testsKernelDevDebugContainer.xml ]; then cp var/cache/dev/testsKernelDevDebugContainer.xml var/cache/dev/testContainer.xml; fi;
+
 echo "Start Web Server"
 php bin/console server:start
