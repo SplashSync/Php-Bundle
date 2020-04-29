161e6cf911616a257e3667eda02e43e1104786d1
BadPixxel <eshop.bpaquier@gmail.com>
2020-04-29 15:38:14 +0200

    FIX: Doctine Object List Helper

--- src/Helpers/Doctrine/ObjectsListHelperTrait.php
+++ src/Helpers/Doctrine/ObjectsListHelperTrait.php
@@ -15,6 +15,7 @@
 
 namespace Splash\Bundle\Helpers\Doctrine;
 
+use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Splash\Client\Splash;
 
@@ -23,6 +24,13 @@ use Splash\Client\Splash;
  */
 trait ObjectsListHelperTrait
 {
+    /**
+     * Default Hydrataion Mode
+     *
+     * @var int
+     */
+    private $hydratationMode = Query::HYDRATE_OBJECT;
+
     /**
      * Return List Of Objects with required filters
      *
@@ -42,22 +50,23 @@ trait ObjectsListHelperTrait
         /** @var QueryBuilder $queryBuilder */
         $queryBuilder = $this->repository->createQueryBuilder('c');
         // Setup Results Offset
-        if (isset($params['offset']) && is_int($params['offset'])) {
-            $queryBuilder->setFirstResult($params['offset']);
+        if (isset($params['offset']) && is_numeric($params['offset'])) {
+            $queryBuilder->setFirstResult((int) $params['offset']);
         }
         // Limit Results Number
-        if (isset($params['max']) && is_int($params['max'])) {
-            $queryBuilder->setMaxResults($params['max']);
+        if (isset($params['max']) && is_numeric($params['max'])) {
+            $queryBuilder->setMaxResults((int) $params['max']);
         }
 
         //====================================================================//
         // Add List Filters
-        if (!empty($filter)) {
+        if (!empty($filter) && method_exists($this, "setObjectListFilter")) {
+            $this->setObjectListFilter($queryBuilder, $filter);
         }
 
         //====================================================================//
         // Load Objects List
-        $rawData = $queryBuilder->getQuery()->getResult();
+        $rawData = $queryBuilder->getQuery()->getResult($this->hydratationMode);
 
         //====================================================================//
         // Parse Data on Result Array
@@ -69,13 +78,27 @@ trait ObjectsListHelperTrait
         //====================================================================//
         // Parse Meta Infos on Result Array
         $response['meta'] = array(
-            'total' => count($rawData),
-            'current' => $this->getTotalCount(),
+            'total' => $this->getTotalCount(),
+            'current' => count($rawData),
         );
 
         //====================================================================//
         // Return result
         return $response;
+    }
+
+    /**
+     * Set Object List Hydrataion Mode
+     *
+     * @param int $hydratationMode
+     *
+     * @return self
+     */
+    protected function setObjectHydratationMode(int $hydratationMode): self
+    {
+        $this->hydratationMode = $hydratationMode;
+
+        return $this;
     }
 
     /**
