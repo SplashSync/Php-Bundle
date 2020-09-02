c35d49ca55ccc9b86f7d492f0a0dd41c08a6bd6c
BadPixxel <eshop.bpaquier@gmail.com>
2020-09-02 22:01:51 +0200

    Update Docs

--- src/Helpers/Doctrine/AbstractEventSubscriber.php
+++ src/Helpers/Doctrine/AbstractEventSubscriber.php
@@ -18,80 +18,23 @@ namespace Splash\Bundle\Helpers\Doctrine;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
-use Exception;
-use Splash\Bundle\Connectors\Standalone;
-use Splash\Bundle\Models\AbstractConnector;
-use Splash\Bundle\Services\ConnectorsManager;
-use Splash\Client\Splash;
-use Splash\Local\Local;
+use Splash\Bundle\Models\AbstractEventSubscriber as BaseAbstractEventSubscriber;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
  * Doctrine Events Subscriber to Listen & Commit Objects Changes
  */
-abstract class AbstractEventSubscriber implements EventSubscriber
+abstract class AbstractEventSubscriber extends BaseAbstractEventSubscriber implements EventSubscriber
 {
     /**
-     * List of Entities Managed by Splash
-     *
-     * @var array
+     * @inheritdoc
      */
-    protected static $entities = array();
-
-    /**
-     * Username used for Commits
-     *
-     * @var string
-     */
-    protected static $username = "Symfony User";
-
-    /**
-     * Username used for Commits
-     *
-     * @var string
-     */
-    protected static $commentPrefix = "Entity";
-
-    /**
-     * Events Triggers States
-     *
-     * @var array
-     */
-    protected static $states = array(
-        Events::postPersist => true,
-        Events::postUpdate => true,
-        Events::preRemove => true,
+    protected static $subscribedEvents = array(
+        Events::postPersist => "postPersist",
+        Events::postUpdate => "postUpdate",
+        Events::preRemove => "preRemove",
     );
-
-    /**
-     * Splash Connectors Manager
-     *
-     * @var ConnectorsManager
-     */
-    private $manager;
-
-    //====================================================================//
-    //  CONSTRUCTOR
-    //====================================================================//
-
-    /**
-     * Service Constructor
-     *
-     * @param ConnectorsManager $manager
-     */
-    public function __construct(ConnectorsManager $manager)
-    {
-        //====================================================================//
-        // Store Faker Connector Manager
-        $this->manager = $manager;
-        //====================================================================//
-        // Setup Events
-        self::setStates(true, true, true);
-        //====================================================================//
-        // Safety Check - Ensure Tracked Entities are Given
-        if (!is_array(static::$entities) || empty(static::$entities)) {
-            throw new Exception("No Tracked Entities Class Defined.");
-        }
-    }
 
     //====================================================================//
     //  Subscriber
@@ -104,31 +47,7 @@ abstract class AbstractEventSubscriber implements EventSubscriber
      */
     public function getSubscribedEvents(): array
     {
-        return array(
-            Events::postPersist => "postPersist",
-            Events::postUpdate => "postUpdate",
-            Events::preRemove => "preRemove",
-        );
-    }
-
-    //====================================================================//
-    //  Module Actions
-    //====================================================================//
-
-    /**
-     * Enable/Disable Events Commits
-     *
-     * @param bool $persist
-     * @param bool $update
-     * @param bool $remove
-     */
-    public static function setStates(bool $persist, bool $update, bool $remove): void
-    {
-        static::$states = array(
-            Events::postPersist => $persist,
-            Events::postUpdate => $update,
-            Events::preRemove => $remove,
-        );
+        return static::$subscribedEvents;
     }
 
     //====================================================================//
@@ -142,7 +61,7 @@ abstract class AbstractEventSubscriber implements EventSubscriber
      */
     public function postPersist(LifecycleEventArgs $eventArgs): void
     {
-        $this->doEventAction(Events::postPersist, $eventArgs, SPL_A_CREATE);
+        $this->doEventAction(Events::postPersist, new GenericEvent($eventArgs->getObject()), SPL_A_CREATE);
     }
 
     /**
@@ -152,7 +71,7 @@ abstract class AbstractEventSubscriber implements EventSubscriber
      */
     public function postUpdate(LifecycleEventArgs $eventArgs): void
     {
-        $this->doEventAction(Events::postUpdate, $eventArgs, SPL_A_UPDATE);
+        $this->doEventAction(Events::postUpdate, new GenericEvent($eventArgs->getObject()), SPL_A_UPDATE);
     }
 
     /**
@@ -162,170 +81,6 @@ abstract class AbstractEventSubscriber implements EventSubscriber
      */
     public function preRemove(LifecycleEventArgs $eventArgs): void
     {
-        $this->doEventAction(Events::preRemove, $eventArgs, SPL_A_DELETE);
-    }
-
-    //====================================================================//
-    //  Private Methods
-    //====================================================================//
-
-    /**
-     * Check if Event Commits are Allowed
-     *
-     * @param string $eventName
-     *
-     * @return bool
-     */
-    public static function isEnabled(string $eventName): bool
-    {
-        if (!isset(static::$states[$eventName])) {
-            return false;
-        }
-
-        return (bool) static::$states[$eventName];
-    }
-
-    /**
-     * Safe Get Event Doctrine Entity Ids
-     * Always returns an array of Object Ids
-     *
-     * @param LifecycleEventArgs $eventArgs
-     * @param AbstractConnector  $connector
-     *
-     * @throws Exception
-     *
-     * @return array
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    protected function getObjectIdentifiers(LifecycleEventArgs $eventArgs, AbstractConnector $connector): array
-    {
-        //====================================================================//
-        // Get Impacted Object Id
-        $entity = $eventArgs->getEntity();
-        //====================================================================//
-        // Safety Check
-        if (!method_exists($entity, "getId")) {
-            throw new Exception("Managed Entity is Invalid, no Id getter exists.");
-        }
-
-        return array((string) $entity->getId());
-    }
-
-    /**
-     * Check if Entity is managed by Splash
-     *
-     * @param LifecycleEventArgs $eventArgs
-     *
-     * @return null|string
-     */
-    private function isManaged(LifecycleEventArgs $eventArgs): ?string
-    {
-        //====================================================================//
-        // Touch Impacted Entity
-        $entity = $eventArgs->getEntity();
-        //====================================================================//
-        // Walk on Managed Entities
-        foreach (static::$entities as $entityClass => $objectType) {
-            if (is_a($entity, $entityClass)) {
-                return $objectType;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * On Entity Created Doctrine Event
-     *
-     * @param string             $eventName
-     * @param LifecycleEventArgs $eventArgs
-     * @param string             $action
-     *
-     * @return void
-     */
-    private function doEventAction(string $eventName, LifecycleEventArgs $eventArgs, string $action): void
-    {
-        //====================================================================//
-        // Check if Event is Enabled
-        if (!$this->isEnabled($eventName)) {
-            return;
-        }
-        //====================================================================//
-        // Check if Entity is Managed by Splash
-        $objectType = $this->isManaged($eventArgs);
-        if (null == $objectType) {
-            return;
-        }
-        //====================================================================//
-        // Do Object Change Commit
-        $this->doCommit($eventArgs, $objectType, $action);
-    }
-
-    /**
-     * Execut Splash Commit for Objects
-     *
-     * @param LifecycleEventArgs $eventArgs
-     * @param string             $objectType
-     * @param string             $action
-     */
-    private function doCommit(LifecycleEventArgs $eventArgs, string $objectType, string $action): void
-    {
-        //====================================================================//
-        //  Search in Configured Servers using Standalone Connector
-        $servers = $this->manager->getConnectorConfigurations(Standalone::NAME);
-        //====================================================================//
-        //  Walk on Configured Servers
-        foreach (array_keys($servers) as $serverId) {
-            //====================================================================//
-            //  Execute Commit to Server
-            $this->doServerCommit($eventArgs, $serverId, $objectType, $action);
-        }
-        //====================================================================//
-        // Catch Splash Logs
-        $this->manager->pushLogToSession(true);
-    }
-
-    /**
-     * Execut Splash Commit for Objects
-     *
-     * @param LifecycleEventArgs $eventArgs
-     * @param string             $serverId
-     * @param string             $objectType
-     * @param string             $action
-     */
-    private function doServerCommit(LifecycleEventArgs $eventArgs, string $serverId, string $objectType, string $action): void
-    {
-        //====================================================================//
-        //  Load Connector
-        $connector = $this->manager->get((string) $serverId);
-        //====================================================================//
-        //  Safety Check
-        if (null === $connector) {
-            return;
-        }
-        //====================================================================//
-        // Setup Splash Local Class
-        $local = Splash::local();
-        if (($local instanceof Local) && empty($local->getServerId())) {
-            $local->setServerId($serverId);
-        }
-        //====================================================================//
-        // Locked (Just created) => Skip
-        if ((SPL_A_UPDATE == $action) && Splash::Object($objectType)->isLocked()) {
-            return;
-        }
-        //====================================================================//
-        //  Transform Entities to Object Ids
-        $objectIds = $this->getObjectIdentifiers($eventArgs, $connector);
-        //====================================================================//
-        // Safety Check
-        if (!is_array($objectIds) || empty($objectIds)) {
-            return;
-        }
-        $commentStr = sprintf("%s %s - %s", static::$commentPrefix, implode(", ", $objectIds), ucfirst($action));
-        //====================================================================//
-        //  Execute Commit
-        $connector->commit($objectType, $objectIds, $action, static::$username, $commentStr);
+        $this->doEventAction(Events::preRemove, new GenericEvent($eventArgs->getObject()), SPL_A_DELETE);
     }
 }
---
+++ src/Models/AbstractEventSubscriber.php
@@ -0,0 +1,338 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2020 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Bundle\Models;
+
+use Doctrine\ORM\Events;
+use Exception;
+use Splash\Bundle\Connectors\Standalone;
+use Splash\Bundle\Services\ConnectorsManager;
+use Splash\Client\Splash;
+use Splash\Local\Local;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * Core Class for Events Subscriber to Listen & Splash Commit Objects Changes
+ */
+abstract class AbstractEventSubscriber
+{
+    /**
+     * List of Entities Managed by Splash
+     * - key:     Class Name
+     * - value:   Splash Object Type Name
+     *
+     * @var array
+     */
+    protected static $classMap = array();
+
+    /**
+     * List of SubscribedEvents
+     *
+     * @var array
+     */
+    protected static $subscribedEvents = array();
+
+    /**
+     * Username used for Commits
+     *
+     * @var string
+     */
+    protected static $username = "Symfony User";
+
+    /**
+     * Username used for Commits
+     *
+     * @var string
+     */
+    protected static $commentPrefix = "Object";
+
+    /**
+     * Events Triggers States
+     *
+     * @var array
+     */
+    protected static $disabledStates = array();
+
+    /**
+     * Splash Connectors Manager
+     *
+     * @var ConnectorsManager
+     */
+    private $manager;
+
+    //====================================================================//
+    //  CONSTRUCTOR
+    //====================================================================//
+
+    /**
+     * Service Constructor
+     *
+     * @param ConnectorsManager $manager
+     */
+    public function __construct(ConnectorsManager $manager)
+    {
+        //====================================================================//
+        // Store Faker Connector Manager
+        $this->manager = $manager;
+        //====================================================================//
+        // Safety Check - Ensure Tracked Entities are Given
+        if (!is_array(static::$classMap) || empty(static::$classMap)) {
+            throw new Exception("No Tracked Objects Class Defined.");
+        }
+        //====================================================================//
+        // Safety Check - Ensure Event Names are Given
+        if (!is_array(static::$subscribedEvents) || empty(static::$subscribedEvents)) {
+            throw new Exception("No Tracked Events Names Defined.");
+        }
+    }
+
+    //====================================================================//
+    //  State Manager
+    //====================================================================//
+
+    /**
+     * Enable/Disable Commits on Given Events Names
+     *
+     * @param string $eventName
+     * @param bool   $status
+     *
+     * @return void
+     */
+    public static function setState(string $eventName, bool $status): void
+    {
+        $eventName = static::class.":".$eventName;
+        if ($status && isset(static::$disabledStates[$eventName])) {
+            unset(static::$disabledStates[$eventName]);
+        }
+
+        if (!$status && !isset(static::$disabledStates[$eventName])) {
+            static::$disabledStates[$eventName] = true;
+        }
+    }
+
+    /**
+     * Enable All Events when Running the Installer
+     */
+    public static function setAllStatesEnabled(): void
+    {
+        foreach (array_keys(static::$subscribedEvents) as $eventName) {
+            static::setState($eventName, true);
+        }
+    }
+
+    /**
+     * Disable All Events when Running the Installer
+     */
+    public static function setAllStatesDisabled(): void
+    {
+        foreach (array_keys(static::$subscribedEvents) as $eventName) {
+            static::setState($eventName, false);
+        }
+    }
+
+    /**
+     * Check if Event Commits are Allowed for an Event name
+     *
+     * @param string $eventName
+     *
+     * @return bool
+     */
+    public static function isStateEnabled(string $eventName): bool
+    {
+        $eventName = static::class.":".$eventName;
+
+        return !isset(static::$disabledStates[$eventName]);
+    }
+
+    //====================================================================//
+    //  Methods to Overrides
+    //====================================================================//
+
+    /**
+     * Detect Object Type from Received Event
+     * Null Types will Filter Events from Beginning
+     *
+     * @param Event $event
+     *
+     * @return null|string
+     */
+    protected function getObjectType(Event $event): ?string
+    {
+        return ($event instanceof GenericEvent)
+            ? self::isInClassMap(get_class($event->getSubject()))
+            : null;
+    }
+
+    /**
+     * Safe Get Event Objects Ids
+     * Always returns an array of Object Ids
+     *
+     * @param Event             $event
+     * @param AbstractConnector $connector
+     *
+     * @throws Exception
+     *
+     * @return array
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function getObjectIdentifiers(Event $event, AbstractConnector $connector): array
+    {
+        //====================================================================//
+        // IF GENERIC EVENT
+        if ($event instanceof GenericEvent) {
+            //====================================================================//
+            // Get Impacted Object Id
+            $object = $event->getSubject();
+            //====================================================================//
+            // Check if Object is Managed
+            if (!self::isInClassMap(get_class($object))) {
+                return array();
+            }
+            //====================================================================//
+            // Safety Check
+            if (!method_exists($object, "getId")) {
+                throw new Exception("Managed Object is Invalid, no Id getter exists.");
+            }
+
+            return array((string) $object->getId());
+        }
+
+        throw new Exception(sprintf("%s Subscribe to a non Generic Event. Please implement an IDs Decoder", __CLASS__));
+    }
+
+    //====================================================================//
+    //  Tooling Methods
+    //====================================================================//
+
+    /**
+     * Check if Object is Managed by Splash
+     *
+     * @param string $className
+     *
+     * @return null|string
+     */
+    protected function isInClassMap(string $className): ?string
+    {
+        //====================================================================//
+        // Walk on Managed Entities
+        foreach (static::$classMap as $entityClass => $objectType) {
+            if (is_a($className, $entityClass, true)) {
+                return $objectType;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * On Entity Created Doctrine Event
+     *
+     * @param string $eventName
+     * @param Event  $event
+     * @param string $action
+     *
+     * @return void
+     */
+    protected function doEventAction(string $eventName, Event $event, string $action): void
+    {
+        //====================================================================//
+        // Check if This Event Should be Triggered
+        if (!static::isStateEnabled($eventName)) {
+            return;
+        }
+        //====================================================================//
+        // Check if Object Type is Valid
+        $objectType = $this->getObjectType($event);
+        if (empty($objectType)) {
+            return;
+        }
+        //====================================================================//
+        // Do Object Change Commit
+        $this->doCommit($event, $objectType, $action);
+    }
+
+    //====================================================================//
+    //  Private Methods
+    //====================================================================//
+
+    /**
+     * Execute Splash Commit for Objects
+     *
+     * @param Event  $event
+     * @param string $objectType
+     * @param string $action
+     */
+    private function doCommit(Event $event, string $objectType, string $action): void
+    {
+        //====================================================================//
+        //  Search in Configured Servers using Standalone Connector
+        $servers = $this->manager->getConnectorConfigurations(Standalone::NAME);
+        //====================================================================//
+        //  Walk on Configured Servers
+        foreach (array_keys($servers) as $serverId) {
+            //====================================================================//
+            //  Execute Commit to Server
+            $this->doServerCommit($event, $serverId, $objectType, $action);
+        }
+        //====================================================================//
+        // Catch Splash Logs
+        $this->manager->pushLogToSession(true);
+    }
+
+    /**
+     * Execut Splash Commit for Objects
+     *
+     * @param Event  $event
+     * @param string $serverId
+     * @param string $objectType
+     * @param string $action
+     */
+    private function doServerCommit(Event $event, string $serverId, string $objectType, string $action): void
+    {
+        //====================================================================//
+        //  Load Connector
+        $connector = $this->manager->get((string) $serverId);
+        //====================================================================//
+        //  Safety Check
+        if (null === $connector) {
+            return;
+        }
+        //====================================================================//
+        // Setup Splash Local Class
+        $local = Splash::local();
+        if (($local instanceof Local) && empty($local->getServerId())) {
+            $local->setServerId($serverId);
+        }
+        //====================================================================//
+        // Locked (Just created) => Skip
+        if ((SPL_A_UPDATE == $action) && Splash::Object($objectType)->isLocked()) {
+            return;
+        }
+        //====================================================================//
+        //  Transform Entities to Object Ids
+        $objectIds = $this->getObjectIdentifiers($event, $connector);
+        //====================================================================//
+        // Safety Check
+        if (!is_array($objectIds) || empty($objectIds)) {
+            return;
+        }
+        $commentStr = sprintf("%s %s - %s", static::$commentPrefix, implode(", ", $objectIds), ucfirst($action));
+        //====================================================================//
+        //  Execute Commit
+        $connector->commit($objectType, $objectIds, $action, static::$username, $commentStr);
+    }
+}
