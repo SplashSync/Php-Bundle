a2e19217b98a5f03a40f5599638c10df50071a0d
BadPixxel <eshop.bpaquier@gmail.com>
2020-10-26 13:15:56 +0100

    FIX: Composer 2

--- src/Resources/travis/configure.sh
+++ src/Resources/travis/configure.sh
@@ -2,8 +2,12 @@ echo -e "\e[104m                                        \e[49m"
 echo -e "\e[104m SPLASH BUNDLE --> Config Travis        \e[49m"
 echo -e "\e[104m                                        \e[49m"
 
-# Update composer
-composer self-update
+# With PHP < 7.3 => Rollback to Composer 1
+if [[ ${TRAVIS_PHP_VERSION:0:3} < "7.3" ]]; then
+    composer self-update --rollback;
+else
+    composer self-update
+fi
 
 # Setup Travis PHP     
 if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then echo "memory_limit = -1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini; fi
