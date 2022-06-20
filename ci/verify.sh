f4859a55f1f90726926c36f64040554e889d1a6c
BadPixxel <eshop.bpaquier@gmail.com>
2022-06-20 12:56:31 +0200

    FIX: Symfony 4 & 5 Compatibility

--- ci/verify.sh
+++ ci/verify.sh
@@ -17,30 +17,27 @@
 ################################################################################
 
 ################################################################################
+# Docker Compose Container you want to check
+CONTAINERS="php-7.4,php-8.0,php-8.1"
+WORKDIR="/var/www/html"
+
+################################################################################
 # Start Docker Compose Stack
 echo '===> Start Docker Stack'
 docker-compose up -d
 
-################################################################################
-# PHP 7.4
-echo '===> Checks Php 7.4'
-docker-compose exec php-7.4 composer update -q
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=phpstan
+######################################
+# Walk on Docker Compose Container
+for ID in $(echo $CONTAINERS | tr "," "\n")
+do
+    echo "===> Checks Php $ID"
 
-################################################################################
-# PHP 8.0
-echo '===> Checks Php 8.0'
-docker-compose exec php-8.0 composer update -q
-docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 8.1
-echo '===> Checks Php 8.1'
-docker-compose exec php-8.1 composer update -q
-docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=phpstan
+    # Ensure Git is Installed
+    docker-compose exec $ID apt install git -y
+    # Run Composer Update
+    docker-compose exec -w $WORKDIR $ID composer update -q
+    # Run Grumphp Test Suites
+    docker-compose exec -w $WORKDIR $ID php vendor/bin/grumphp run --testsuite=travis
+    docker-compose exec -w $WORKDIR $ID php vendor/bin/grumphp run --testsuite=csfixer
+    docker-compose exec -w $WORKDIR $ID php vendor/bin/grumphp run --testsuite=phpstan
+done
