106f67218e793c680fa3e1ce59eca5a5e9f92dc5
BadPixxel <eshop.bpaquier@gmail.com>
2022-10-04 16:55:17 +0200

    WIP: Add Field Index Flag

--- src/Interfaces/Connectors/PrimaryKeysInterface.php
+++ src/Interfaces/Connectors/PrimaryKeysInterface.php
@@ -40,7 +40,7 @@ interface PrimaryKeysInterface
      *
      * @return null|string
      *
-     * @remark  This Feature is Optional but Highly recommended for
+     * @note  This Feature is Optional but Highly recommended for
      *          Objects alike Products(SKU), Users (Email), and more...
      *
      * @since 2.0.0
