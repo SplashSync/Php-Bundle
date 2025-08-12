536d328231a406e490a68637db673bd19e5e6860
BadPixxel <eshop.bpaquier@gmail.com>
2025-08-12 18:10:27 +0200

    Introduce `StandaloneServiceTags` for centralized service tag management
    Added a new class defining constants for standalone connector service tags including object, action, widget, and extension mappings to improve code consistency and reusability.

---
+++ src/Dictionary/StandaloneServiceTags.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Bundle\Dictionary;
+
+/**
+ * Dictionary for Splash Standalone Connector Services Tags
+ */
+class StandaloneServiceTags
+{
+    /**
+     * Standalone Connector Service Tag
+     */
+    const ID = "splash.connectors.standalone";
+
+    /**
+     * Tag a Service as a Splash Standalone Object
+     */
+    const OBJECT = "splash.standalone.object";
+
+    /**
+     * Tag a Service as a Splash Standalone Object Action
+     */
+    const ACTION = "splash.standalone.action";
+
+    /**
+     * Tag a Service as a Splash Standalone Object Extension
+     */
+    const EXTENSION = "splash.standalone.extension";
+
+    /**
+     * Tag a Service as a Splash Standalone Widget
+     */
+    const WIDGET = "splash.standalone.widget";
+}
